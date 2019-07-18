3599b34bdcc3ffde592ad2f4614f205f6978badb
Hwang eui song <47591345+dmlthd1995@users.noreply.github.com>
2019-07-19 01:10:37 +0900

    Create AR_marker_scan1.py

---
+++ AR_marker_scan1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+try:
+	import cv2
+	from ar_markers import detect_markers
+except ImportError:
+	raise Exception('Error: OpenCv is not installed')
+
+
+if __name__ == '__main__':
+	print('Press "q" to quit')
+	capture = cv2.VideoCapture(0)
+
+	if capture.isOpened():  # try to get the first frame
+		frame_captured, frame = capture.read()
+	else:
+		frame_captured = False
+	while frame_captured:
+		markers = detect_markers(frame)
+		for marker in markers:
+			marker.highlite_marker(frame)
+			#print(marker)
+			print ('Marker ID:',marker.id)
+		cv2.imshow('Test Frame', frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+		#print (markers)
+		frame_captured, frame = capture.read()
+		#print markers
+	# When everything done, release the capture
+	capture.release()
+	cv2.destroyAllWindows()
